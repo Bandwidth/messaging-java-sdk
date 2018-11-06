6b28a6a25e6c090ef7993444fb49ed712f89f8b9
jmulford-bandwidth <jmulford@bandwidth.com>
2018-11-06 14:41:46 -0500

    Added feedback from PR

--- src/main/java/com/bandwidth/sdk/messaging/BandwidthMessagingException.java
+++ src/main/java/com/bandwidth/sdk/messaging/BandwidthMessagingException.java
@@ -1,49 +1,18 @@
 package com.bandwidth.sdk;
 
 
-public class BandwidthMessagingException extends RuntimeException {
-
-    private static final String MESSAGE_ERROR_TYPE = "message-failed";
+public class MessagingException extends RuntimeException {
     
-    private int errorCode;
-    private MessageEvent message;
 
-
-    public BandwidthMessagingException(String message) {
+    public MessagingException(String message) {
         super(message);
     }
 
-    public BandwidthMessagingException(Throwable cause) {
+    public MessagingException(Throwable cause) {
         super(cause);
     }
 
-    public BandwidthMessagingException(String message, Throwable cause) {
+    public MessagingException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public BandwidthMessagingException(String message, int errorCode, Throwable cause) {
-        super(message, cause);
-        this.errorCode = errorCode;
-    }
-
-    public BandwidthMessagingException(String message, MessageEvent messageEvent, Throwable cause) {
-        super(message, cause);
-        this.messageEvent = messageEvent;
-        if (messageEvent.getErrorCode().isPresent()) { //no reason this should not be the case
-            this.errorCode = messageEvent;
-        }
-    }
-
-    /**
-     * Specifics for error codes can be found here https://dev.bandwidth.com/v2-messaging/codes.html
-     */
-    public MessageErrorTypes getErrorType(){
-        return getErrorCode().map(errorCode -> {
-            if (4000 <= errorCode && errorCode < 5000)
-                return MessageErrorTypes.CLIENT;
-            else if (5000 <= errorCode && errorCode < 6000)
-                return MessageErrorTypes.SERVER;
-            else return MessageErrorTypes.UNKNOWN;
-        });
     }
 }
