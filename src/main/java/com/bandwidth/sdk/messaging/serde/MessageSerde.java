df3a74e5fcc118d748eac641458c44c68842fb9e
John Hudson <jhudson@bandwidth.com>
2018-11-07 16:46:08 -0500

    fix callback list parsing

--- src/main/java/com/bandwidth/sdk/messaging/serde/MessageSerde.java
+++ src/main/java/com/bandwidth/sdk/messaging/serde/MessageSerde.java
@@ -1,15 +1,20 @@
 package com.bandwidth.sdk.messaging.serde;
 
+import static com.bandwidth.sdk.messaging.exception.ExceptionUtils.catchClientExceptions;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-
-import static com.bandwidth.sdk.messaging.exception.ExceptionUtils.catchClientExceptions;
 
 public class MessageSerde {
     private static final ObjectMapper mapper = new ObjectMapper()
             .registerModule(new GuavaModule())
             .registerModule(new Jdk8Module());
+
+    public <T> T deserialize(String messageBody, TypeReference<T> clazz) {
+        return catchClientExceptions(() -> mapper.readValue(messageBody, clazz));
+    }
 
     public <T> T deserialize(String messageBody, Class<T> clazz) {
         return catchClientExceptions(() -> mapper.readValue(messageBody, clazz));
